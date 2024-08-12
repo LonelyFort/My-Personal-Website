958f074a1752f894e273b0977991131a4ec10b65
Monoe <jeffliu5925b@gmail.com>
2024-08-12 18:28:45 +0800

    small fix

--- src/components/Pages/Projects/ProjectData.jsx
+++ src/components/Pages/Projects/ProjectData.jsx
@@ -7,47 +7,5 @@ export const ProjectData = [
         media: Project1,
         link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
         techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
-    },
-    {
-        title: "Confirmation Email on Telegram Bot",
-        summary: "This Web application intends to mimic sending a confirmation email after users, specifically from NUS, \n typed in their NUS email address into the form.",
-        media: Project1,
-        link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
-        techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
-    },
-    {
-        title: "Confirmation Email on Telegram Bot",
-        summary: "This Web application intends to mimic sending a confirmation email after users, specifically from NUS, \n typed in their NUS email address into the form.",
-        media: Project1,
-        link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
-        techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
-    },
-    {
-        title: "Confirmation Email on Telegram Bot",
-        summary: "This Web application intends to mimic sending a confirmation email after users, specifically from NUS, \n typed in their NUS email address into the form.",
-        media: Project1,
-        link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
-        techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
-    },
-    {
-        title: "Confirmation Email on Telegram Bot",
-        summary: "This Web application intends to mimic sending a confirmation email after users, specifically from NUS, \n typed in their NUS email address into the form.",
-        media: Project1,
-        link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
-        techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
-    },
-    {
-        title: "Confirmation Email on Telegram Bot",
-        summary: "This Web application intends to mimic sending a confirmation email after users, specifically from NUS, \n typed in their NUS email address into the form.",
-        media: Project1,
-        link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
-        techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
-    },
-    {
-        title: "Confirmation Email on Telegram Bot",
-        summary: "This Web application intends to mimic sending a confirmation email after users, specifically from NUS, \n typed in their NUS email address into the form.",
-        media: Project1,
-        link: "https://github.com/LonelyFort/confirmation-email-on-tele-miniapp",
-        techstack: "Tech stack used: React, TypeScript, Resend, Python, Vercel"
     }
 ]
